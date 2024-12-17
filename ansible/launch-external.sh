81806e439217fd57a7407fd284df8de13f5fab81
papum <danieledugo1@gmail.com>
2024-12-17 18:34:10 +0100

    vm image is already prepopulated and has ranflood_5_9.exe; todo: run ranfloodd, ransomware, check checksums; then sss

--- ansible/launch-external.sh
+++ ansible/launch-external.sh
@@ -4,7 +4,24 @@
 source variables.sh
 
 # create variables for windows (remote)
-sed -r 's/(.*=.*)/set \1/; s/\$\{([^}]*)\}/%\1%/g' ${name_windows_env} > ${name_windows_env}
+#sed -r 's/(.*=.*)/set \1/; s/\$\{([^}]*)\}/%\1%/g' ${name_windows_env} > ${name_windows_env}
 
+
+# Downloads
+
+mkdir -p ${local_working_directory}/${local_ranflood_files_dir}
+cd ${local_working_directory}/${local_ranflood_files_dir}
+
+if [[ ! -f ${ranflood_zip_win} ]]; then
+	echo "[Download] Downloading ranflood..."
+	curl -L -o ${name_ranflood_zip} ${ranflood_download}/${ranflood_version}/${ranflood_zip_win}
+else
+	echo "[Download] Ranflood already downloaded"
+fi
+
+cd ${local_working_directory}
+
+
+# Run ansible
 
 ansible-playbook -i external_inventory full_playbook.yml --extra-vars "@ansible_variables.yml"
