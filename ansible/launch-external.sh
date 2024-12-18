c1d095d9ff7f425d0fe2fa6778cd0f9cf1346420
papum_debian <danieledugo1@gmail.com>
2024-12-18 11:11:26 +0100

    tests on usable folders

--- ansible/launch-external.sh
+++ ansible/launch-external.sh
@@ -12,12 +12,12 @@ source variables.sh
 mkdir -p ${local_working_directory}/${local_ranflood_files_dir}
 cd ${local_working_directory}/${local_ranflood_files_dir}
 
-if [[ ! -f ${ranflood_zip_win} ]]; then
-	echo "[Download] Downloading ranflood..."
-	curl -L -o ${name_ranflood_zip} ${ranflood_download}/${ranflood_version}/${ranflood_zip_win}
-else
-	echo "[Download] Ranflood already downloaded"
-fi
+#if [[ ! -f ${ranflood_zip_win} ]]; then
+#	echo "[Download] Downloading ranflood..."
+#	curl -L -o ${name_ranflood_zip} ${ranflood_download}/${ranflood_version}/${ranflood_zip_win}
+#else
+#	echo "[Download] Ranflood already downloaded"
+#fi
 
 cd ${local_working_directory}
 
